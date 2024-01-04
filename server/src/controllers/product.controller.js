3a72323fc47640da8273a840d6c842ccfb906e5a
Subhadeep <subhadeepsarkar1995@gmail.com>
2024-01-04 17:09:58 +0530

    added api end points for full CRUD of products

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -10,6 +10,68 @@ const createProduct = async (req, res) => {
   }
 }
 
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find()
+    if (products.length) {
+      res
+        .status(200)
+        .send({ message: `${products.length} products found`, products })
+    } else {
+      res.status(404).send({ message: "No product found" })
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ error: `${error.message}` })
+  }
+}
+
+const getSingleProduct = async (req, res) => {
+  try {
+    const { id } = req.params
+    const product = await Product.findOne({ _id: id })
+    if (product) {
+      res.status(200).send({ message: `Product found`, product })
+    } else {
+      res.status(200).send({ message: "Product not found" })
+    }
+  } catch (error) {
+    res.status(500).send({ error: `${error.message}` })
+  }
+}
+
+const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params
+    const product = await Product.findByIdAndUpdate(
+      id,
+      { ...req.body },
+      { new: true }
+    )
+    if (product) {
+      res.status(200).send({ message: "Product updated", product })
+    } else {
+      res.status(400).send({ error: "Error in updating product" })
+    }
+  } catch (error) {
+    res.status(500).send({ error: `${error.message}` })
+  }
+}
+
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params
+    const product = await Product.findByIdAndDelete(id)
+    res.status(200).send({ message: `Product deleted` })
+  } catch (error) {
+    res.status(500).send({ error: `${error.message}` })
+  }
+}
+
 module.exports = {
   createProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
 }
--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -1,7 +1,17 @@
 const express = require("express")
-const { createProduct } = require("../controllers/product.controller")
+const {
+  createProduct,
+  getAllProducts,
+  getSingleProduct,
+  deleteProduct,
+  updateProduct,
+} = require("../controllers/product.controller")
 const productRouter = express.Router()
 
 productRouter.post("/", createProduct)
+productRouter.get("/", getAllProducts)
+productRouter.get("/:id", getSingleProduct)
+productRouter.patch("/:id", updateProduct)
+productRouter.delete("/:id", deleteProduct)
 
 module.exports = productRouter
